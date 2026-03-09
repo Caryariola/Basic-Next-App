0bebb11f5872f2698e1805ac98f32765d42b229b
Cary Jr. Gomez Ariola <carygomezariola@gmail.com>
2026-03-09 14:03:28 +0800

    ssl

--- lib/db.ts
+++ lib/db.ts
@@ -5,7 +5,9 @@ const globalForPool = global as unknown as { pool: Pool };
 export const pool = globalForPool.pool || new Pool({
   connectionString: process.env.DATABASE_URL,
   
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false
+  },
 
   max: 20,
   idleTimeoutMillis: 30000,
